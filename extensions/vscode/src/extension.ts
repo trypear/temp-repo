b3777f438925497ceef3db80c1af4181a0e61b00
nang-dev <contactnang88@gmail.com>
2024-10-04 22:18:59 -0700

    Added matching

--- extensions/vscode/src/extension.ts
+++ extensions/vscode/src/extension.ts
@@ -21,7 +21,7 @@ async function dynamicImportAndActivate(context: vscode.ExtensionContext) {
       )
       .then((selection) => {
         if (selection === "View Logs") {
-          vscode.commands.executeCommand("continue.viewLogs");
+          vscode.commands.executeCommand("pearai.viewLogs");
         } else if (selection === "Retry") {
           // Reload VS Code window
           vscode.commands.executeCommand("workbench.action.reloadWindow");
@@ -29,6 +29,8 @@ async function dynamicImportAndActivate(context: vscode.ExtensionContext) {
       });
   }
 }
+
+
 
 export function activate(context: vscode.ExtensionContext) {
   setupCa();
